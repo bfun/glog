e5f5660ade681076efbc1fc27a7a57051d14c0d3
nil <14428376+bitless@user.noreply.gitee.com>
2025-03-11 09:25:22 +0800

    v0.0.2

--- log.go
+++ log.go
@@ -8,67 +8,32 @@ import (
 	"time"
 )
 
-var Size int64 = 1024 * 1024 * 10
-var fileName string
 var m sync.Mutex
-var count int
+var logDir = "log"
+var dirPerm os.FileMode = 0755
+var filePerm os.FileMode = 0644
 
-func newName(bak bool) string {
-	prefix := "log/" + path.Base(os.Args[0]) + ".log."
-	format := "20060102"
-	if bak {
-		format = "20060102-150405.000"
-	}
-	return prefix + time.Now().Format(format)
-}
-
-func replaceAttr(groups []string, a slog.Attr) slog.Attr {
-	if a.Key != "time" {
-		return a
-	}
-	a = slog.String("time", time.Now().Format("2006-01-02 15:04:05.000000"))
-	s1 := newName(false)
-	if s1 != fileName {
-		slog.SetDefault(newLogger())
-		return a
-	}
-	if count < 1000 {
-		count++
-		return a
-	}
-	count = 0
-	stat, err := os.Stat(fileName)
-	if err != nil {
-		panic(err)
-	}
-	if stat.Size() < Size {
-		return a
-	}
-	s2 := newName(true)
-	err = os.Rename(fileName, s2)
-	if err != nil {
-		panic(err)
-	}
-	slog.SetDefault(newLogger())
-	return a
+func newName() string {
+	format := "20060102-150405"
+	baseName := path.Base(os.Args[0]) + ".log." + time.Now().Format(format)
+	return path.Join(logDir, baseName)
 }
 
 func newLogger() *slog.Logger {
 	m.Lock()
 	defer m.Unlock()
-	err := os.Mkdir("log", 0755)
+	err := os.Mkdir(logDir, dirPerm)
 	if err != nil {
 		if !os.IsExist(err) {
 			panic(err)
 		}
 	}
-	fileName = newName(false)
-	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	fileName := newName()
+	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, filePerm)
 	if err != nil {
 		panic(err)
 	}
-	o := slog.HandlerOptions{ReplaceAttr: replaceAttr}
-	h := slog.NewTextHandler(f, &o)
+	h := slog.NewTextHandler(f, nil)
 	return slog.New(h)
 }
 
